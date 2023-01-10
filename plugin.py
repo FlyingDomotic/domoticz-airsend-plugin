ff21761c0fe86e6578cce431256a157a5ada30f4
Flying Domotic <71657879+FlyingDomotic@users.noreply.github.com>
2023-01-10 21:48:57 +0100

    Log commands send to AirSend box

--- plugin.py
+++ plugin.py
@@ -1,6 +1,6 @@
 #           AirSend plugin
 """
-<plugin key="AirSend" name="AirSend plugin" author="Flying Domotic" version="0.0.12">
+<plugin key="AirSend" name="AirSend plugin" author="Flying Domotic" version="0.0.13">
     <description>
       AirSend plug-in from Flying Domotic<br/><br/>
       Integrates AirSend devices into Domoticz<br/>
@@ -489,6 +489,7 @@ class BasePlugin:
         else:
             Domoticz.Error('Command: "' + str(Command) + '" not supported yet for ' + device.Name+'. Please ask for support.')
         if airSendType != -1:
+            Domoticz.Log 'Sending notes type='+str(airSendType)+', value='+str(airSendValue)+ ' to channel Id='+elements[0]+', source='+elements[1])
             elements = device.DeviceID.split('/')
             jsonData = '{"wait": true, "channel": {"id":"'+elements[0]+'","source":"'+elements[1]+'"}, "thingnotes":{"notes":[{"method":1,"type":'+str(airSendType)+',"value":'+str(airSendValue)+'}]}}'
             localUrl = self.webServiceUrl+'airsend/transfer'
